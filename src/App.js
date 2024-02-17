c40a698b9414d02b6d327b0d125a652fa1e85cad
Rakshitha-05 <prakshitha455@gmail.co>
2024-02-17 18:34:03 +0530

    Styled ‘Header’ (Inline Styling)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 
 import React, { Component } from 'react';
 
-
+import Header from "./Header"
 
 class App extends Component{
   render() {
+    // let x = 10;
+    // let y = 20;
     return (
       <div>
-        PHONE DIRECTORY
+      <Header></Header>
+      <button>ADD</button>
+      <div>
+        <span> Name</span><br/>
+        <span> Phone</span>
+      </div>
+      {/* <label htmlfor="name">ADDITION VALUE: </label>
+      <input id="name" type="text" placeholder="Type Here" defaultValue={x+y}></input> */}
+
+
       </div>
     );
      
---
+++ src/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+
+const Header = function(){
+    const headerStyle = {textAlign: 'center', padding: 20, background: '#000', color:'#fff', textTransform: 'uppercase'}
+    return(
+            <div>
+                <div style = {headerStyle}>
+                    PHONE DIRECTORY
+                </div>    
+            </div>
+
+    );
+}
+
+
+
+
+
+
+export default Header;
+   
