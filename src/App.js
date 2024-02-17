c7d9a64082bda432a95bb9cdee37d45b20b46757
Rakshitha-05 <prakshitha455@gmail.co>
2024-02-17 23:06:50 +0530

    Rendered Subscribers Dynamically

--- src/App.js
+++ src/App.js
@@ -3,22 +3,53 @@ import React, { Component } from 'react';
 
 import Header from "./Header"
 
+import './App.css'
+
 class App extends Component{
+    
+
   render() {
     // let x = 10;
     // let y = 20;
+    let subscribers = [
+      {
+        id: 1,
+        name: "Rakshitha",
+        phone: "7987979872"
+      },
+      {
+        id: 2,
+        name: "Rakshiv",
+        phone: "7987979872"
+      }
+
+    ]
+
     return (
-      <div>
+      <div className="component-container">
       <Header></Header>
-      <button>ADD</button>
+      <div className="component-body-container">
+      <button className="custom-btn add-btn">ADD</button>
       <div>
-        <span> Name</span><br/>
-        <span> Phone</span>
+        <div className="grid-container heading-container"></div>
+        <span className="grid-item name-heading"> Name</span>
+        <span className="grid-item phone-heading"> Phone</span>
       </div>
       {/* <label htmlfor="name">ADDITION VALUE: </label>
       <input id="name" type="text" placeholder="Type Here" defaultValue={x+y}></input> */}
 
+      {
+        subscribers.map(sub => {
+          return <div key={sub.id} className="grid-container">
+          <span className="grid-item">{sub.name}</span>
+          <span className="grid-item">{sub.phone}</span>
+          </div>
+        })
+      }
 
+
+
+      </div>
       </div>
     );
      
